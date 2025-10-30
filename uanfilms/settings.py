0a6deab223c796c465ba0fc6419ae560fcab2ab9
jamessarmiento13 <jasarmiento13@uan.edu.co>
2025-10-30 15:43:10 -0500

    Cambio de base de datos a SQLite para despliegue gratuito

--- uanfilms/settings.py
+++ uanfilms/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'uanfilms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.getenv('DB_NAME', BASE_DIR / 'db.sqlite3'),
     }
 }
 
